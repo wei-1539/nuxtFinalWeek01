7ddb6d511a955c539b5ea45ce5d525eeffba926a
Rio Wei <wutony0713@gmail.com>
2024-11-22 23:19:31 +0800

    新增404頁面

---
+++ pages/[...slug].vue
@@ -0,0 +1,16 @@
+<script setup>
+const route = useRoute()
+const router =useRouter()
+
+definePageMeta({
+    layout:'noPage'
+})
+</script>
+<template>
+  <div>
+    <h1>404</h1>
+    <h2>目前沒有這個 {{ route.fullPath }} 的 URL 唷</h2>
+    <button type="button" @click="router.push('/')">回到首頁</button>
+  </div>
+</template>
+<style scoped></style>
